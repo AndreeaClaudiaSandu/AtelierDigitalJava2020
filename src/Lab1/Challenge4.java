d988d2681a6e240428cce5487ad7e92fb68a7f8b
AndreeaClaudiaSandu <asandu766@gmail.com>
2020-11-23 20:06:20 +0200

    prima problema

--- src/Lab1/Challenge4.java
+++ src/Lab1/Challenge4.java
@@ -10,7 +10,7 @@ public class Challenge4 {
             for (int j = i + 1; j < numere.length-1; j++) {
                 for(int k= j+1; k<numere.length;k++) {
                     if (numere[i] + numere[j] +numere[k] == 0) {
-                        if (folosire[i] == 0 && folosire[j] == 0 && folosire[k]==0) {
+                        if (folosire[i] == 0 & folosire[j] == 0 & folosire[k]==0) {
                             nr++;
                             folosire[i] = 1;
                             folosire[j] = 1;
---
+++ src/lab2/BoxingMatch.java
@@ -0,0 +1,20 @@
+package lab2;
+
+public class BoxingMatch {
+    private Fighter f1;
+    private Fighter f2;
+    public BoxingMatch(Fighter f1, Fighter f2){
+        this.f1=f1;
+        this.f2=f2;
+    }
+
+    public String Fight(){
+        while(f1.getHealth() >0 && f2.getHealth()>0){
+            f1.attack(f2);
+            if(f2.getHealth()<=0)
+                return f1.getName();
+            f2.attack(f1);
+        }
+        return f2.getName();
+    }
+}
